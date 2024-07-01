808ec9152268e652b619fdd5924e877868dbc6a1
Gabriel Reis <gabsrs@gitbasic.com>
2024-07-01 19:42:07 -0300

    fix: chage handle body data - Change from express.json() to express.urlencoded()

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { router } from "./routes/index.js"
 
 const app = express();
 
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 const __dirname = fileURLToPath(import.meta.url)
 
